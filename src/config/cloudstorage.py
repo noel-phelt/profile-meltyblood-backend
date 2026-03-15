15ffde46affe7e20aa891e2c235839fa72eb8e3b
takaya-n <service_nt@icloud.com>
2026-03-15 17:38:57 +0900

    Use ADC for Firebase and Cloud Storage

--- src/config/cloudstorage.py
+++ src/config/cloudstorage.py
@@ -2,18 +2,13 @@ import uuid
 
 from fastapi import UploadFile
 from google.cloud import storage
-from google.oauth2 import service_account
 
-from config.env import FIREBASE_CERT, GCP
-
-credential = service_account.Credentials.from_service_account_info(
-    FIREBASE_CERT)
+from config.env import GCP
 
 
 class CloudStorage:
     def __init__(self):
-        self.client = storage.Client(
-            project=GCP['project_id'], credentials=credential)
+        self.client = storage.Client(project=GCP['project_id'])
         self.bucket = self.client.get_bucket(GCP['bucket_name'])
 
     def upload_profile(self, profile_image: UploadFile) -> str:
